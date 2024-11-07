654c9c148f526bf9c5e9ff47637e2213d7d4e9fc
Artem Stepanov <ST3PNV@gmail.com>
2024-11-07 11:56:46 +0300

    Time: 1 ms (76.99%), Space: 10.5 MB (70.04%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -6,11 +6,9 @@ public:
         string piece;
         for (int i = 0; i < path.size(); i++) {
             if (path[i] == '/') {
-                if (piece == "..") {
-                    if (!dirs.empty()) dirs.pop_back();
-                } else {
-                    if (piece != "." && piece != "") dirs.push_back(piece);
-                }
+                if (piece == "." || piece == "" || (piece == ".." && dirs.empty())) piece = "";
+                else if (piece == "..") dirs.pop_back();
+                else dirs.push_back(piece);
                 piece = "";
             } else {
                 piece += path[i];
